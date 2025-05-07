45d17e1d32067e299b904a2a425cfc531c1892a3
feibis <fabiansang@gmail.com>
2025-05-07 17:45:17 +0800

    the

--- components/marketing/sections/hero.tsx
+++ components/marketing/sections/hero.tsx
@@ -41,13 +41,13 @@ function HeroPill(): React.JSX.Element {
           className="group h-8 rounded-full px-3 text-xs font-medium shadow-sm duration-200 hover:bg-accent/50 sm:text-sm"
         >
           <div className="w-fit py-0.5 text-center text-xs text-blue-500 sm:text-sm">
-            New!
+            Free consult!🎉
           </div>
           <Separator
             orientation="vertical"
             className="mx-2"
           />
-          Put an announcement here 🎉
+          Whatsapp +48 797264600
           <ChevronRightIcon className="ml-1.5 size-3 shrink-0 text-foreground transition-transform group-hover:translate-x-0.5" />
         </Badge>
       </Link>
@@ -63,8 +63,8 @@ function HeroTitle(): React.JSX.Element {
       transition={{ delay: 0.2, duration: 0.4 }}
     >
       <h1 className="mt-6 text-center text-[48px] font-bold leading-[54px] tracking-[-1.2px] [font-kerning:none] sm:text-[56px] md:text-[64px] lg:text-[76px] lg:leading-[74px] lg:tracking-[-2px]">
-        Your revolutionary
-        <br /> Next.js SaaS
+        Fast third-party 
+        <br /> China tasks assistant
       </h1>
     </motion.div>
   );
@@ -78,8 +78,7 @@ function HeroDescription(): React.JSX.Element {
       transition={{ delay: 0.4, duration: 0.4 }}
       className="mx-auto mt-3 max-w-[560px] text-balance text-center text-lg leading-[26px] text-muted-foreground sm:text-xl lg:mt-6"
     >
-      This is a demo application built with Achromatic. It will save you time
-      and effort building your next SaaS.
+      ChinaTasks simplifies the small stuff—suppliers, payments, logistics, pros. Your China fixer, done right.
     </motion.p>
   );
 }
@@ -112,7 +111,7 @@ function HeroButtons(): React.JSX.Element {
           'h-10 rounded-xl sm:h-9'
         )}
       >
-        Talk to sales
+        Talk to US
       </Link>
     </motion.div>
   );
@@ -191,161 +190,6 @@ function SupportiveDashedGridLines(): React.JSX.Element {
   );
 }
 
-function HeroIllustration(): React.JSX.Element {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.6, duration: 0.4 }}
-      className="relative mt-3 lg:mt-6"
-    >
-      <SupportiveDashedGridLines />
-      <UnderlinedTabs
-        defaultValue="feature1"
-        className="mt-3"
-      >
-        <ScrollArea className="max-w-[100vw] lg:max-w-none">
-          <UnderlinedTabsList className="relative z-20 mb-6 flex h-fit flex-row flex-wrap justify-center md:flex-nowrap">
-            <UnderlinedTabsTrigger
-              value="feature1"
-              className="mx-1 px-2.5 sm:mx-2 sm:px-3"
-            >
-              <CubeIcon className="mr-2 size-4 shrink-0" />
-              Feature 1
-            </UnderlinedTabsTrigger>
-            <UnderlinedTabsTrigger
-              value="feature2"
-              className="mx-1 px-2.5 sm:mx-2 sm:px-3"
-            >
-              <PlayIcon className="mr-2 size-4 shrink-0" />
-              Feature 2
-            </UnderlinedTabsTrigger>
-            <UnderlinedTabsTrigger
-              value="feature3"
-              className="mx-1 px-2.5 sm:mx-2 sm:px-3"
-            >
-              <CircuitBoardIcon className="mr-2 size-4 shrink-0" />
-              Feature 3
-            </UnderlinedTabsTrigger>
-            <UnderlinedTabsTrigger
-              value="feature4"
-              className="mx-1 px-2.5 sm:mx-2 sm:px-3"
-            >
-              <LayoutIcon className="mr-2 size-4 shrink-0" />
-              Feature 4
-            </UnderlinedTabsTrigger>
-            <UnderlinedTabsTrigger
-              value="feature5"
-              className="mx-1 px-2.5 sm:mx-2 sm:px-3"
-            >
-              <FileBarChartIcon className="mr-2 size-4 shrink-0" />
-              Feature 5
-            </UnderlinedTabsTrigger>
-          </UnderlinedTabsList>
-          <ScrollBar
-            orientation="horizontal"
-            className="invisible"
-          />
-        </ScrollArea>
-        <div className="relative mb-1 w-full rounded-xl dark:border-none dark:bg-background">
-          <SupportiveDashedGridLines />
-          <div className="relative z-20 bg-background">
-            <UnderlinedTabsContent value="feature1">
-              <Image
-                quality={100}
-                src="/marketing/hero/light-feature1.webp"
-                width="1328"
-                height="727"
-                alt="Feature 1 screenshot"
-                className="block rounded-xl border shadow dark:hidden"
-              />
-              <Image
-                quality={100}
-                src="/marketing/hero/dark-feature1.webp"
-                width="1328"
-                height="727"
-                alt="Feature 1 screenshot"
-                className="hidden rounded-xl border shadow dark:block"
-              />
-            </UnderlinedTabsContent>
-            <UnderlinedTabsContent value="feature2">
-              <Image
-                quality={100}
-                src="/marketing/hero/light-feature2.webp"
-                width="1328"
-                height="727"
-                alt="Feature 2 screenshot"
-                className="block rounded-xl border shadow dark:hidden"
-              />
-              <Image
-                quality={100}
-                src="/marketing/hero/dark-feature2.webp"
-                width="1328"
-                height="727"
-                alt="Feature 2 screenshot"
-                className="hidden rounded-xl border shadow dark:block"
-              />
-            </UnderlinedTabsContent>
-            <UnderlinedTabsContent value="feature3">
-              <Image
-                quality={100}
-                src="/marketing/hero/light-feature3.webp"
-                width="1328"
-                height="727"
-                alt="Feature 3 screenshot"
-                className="block rounded-xl border shadow dark:hidden"
-              />
-              <Image
-                quality={100}
-                src="/marketing/hero/dark-feature3.webp"
-                width="1328"
-                height="727"
-                alt="Feature 3 screenshot"
-                className="hidden rounded-xl border shadow dark:block"
-              />
-            </UnderlinedTabsContent>
-            <UnderlinedTabsContent value="feature4">
-              <Image
-                quality={100}
-                src="/marketing/hero/light-feature4.webp"
-                width="1328"
-                height="727"
-                alt="Feature 4 screenshot"
-                className="block rounded-xl border shadow dark:hidden"
-              />
-              <Image
-                quality={100}
-                src="/marketing/hero/dark-feature4.webp"
-                width="1328"
-                height="727"
-                alt="Feature 4 screenshot"
-                className="hidden rounded-xl border shadow dark:block"
-              />
-            </UnderlinedTabsContent>
-            <UnderlinedTabsContent value="feature5">
-              <Image
-                quality={100}
-                src="/marketing/hero/light-feature5.webp"
-                width="1328"
-                height="727"
-                alt="Feature 5 screenshot"
-                className="block rounded-xl border shadow dark:hidden"
-              />
-              <Image
-                quality={100}
-                src="/marketing/hero/dark-feature5.webp"
-                width="1328"
-                height="727"
-                alt="Feature 5 screenshot"
-                className="hidden rounded-xl border shadow dark:block"
-              />
-            </UnderlinedTabsContent>
-          </div>
-        </div>
-      </UnderlinedTabs>
-    </motion.div>
-  );
-}
 
 export function Hero(): React.JSX.Element {
   return (
