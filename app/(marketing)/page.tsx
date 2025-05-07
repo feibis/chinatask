4ad56cba5cc6b18af930b323d49e945d9af60962
feibis <fabiansang@gmail.com>
2025-05-07 19:15:52 +0800

    thehehrhetr

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { CTA } from '@/components/marketing/sections/cta';
 import { FAQ } from '@/components/marketing/sections/faq';
 import { Hero } from '@/components/marketing/sections/hero';
-import { Logos } from '@/components/marketing/sections/logos';
 import { Problem } from '@/components/marketing/sections/problem';
 import { Solution } from '@/components/marketing/sections/solution';
 import { Stats } from '@/components/marketing/sections/stats';
